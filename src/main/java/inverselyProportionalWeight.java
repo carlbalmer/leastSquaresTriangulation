31d0ea1314da87da5bb8be542dcab7c4f3b5b993
Carl Balmer <carl.balmer@gmail.com>
2016-08-16 17:01:01 +0200

    Implemented the Weight interface in inverselyProportionalWeight Added the JFreeChart library. Added a main method in lsTriangulation

--- src/main/java/inverselyProportionalWeight.java
+++ src/main/java/inverselyProportionalWeight.java
@@ -1,14 +1,25 @@
 import java.util.LinkedList;
 
 /**
+ * Assigns weights to the distances based on the formula di = di/sum(di)
  * Created by carl on 16.08.16.
  */
 public class inverselyProportionalWeight implements Weight {
     public LinkedList<Datapoint> assignWeights(LinkedList<Datapoint> datapoints) {
-        return null;
+        LinkedList<Datapoint> output = new LinkedList<Datapoint>();
+        while (!datapoints.isEmpty()) {
+            output.add(assignWeights(datapoints.remove()));
+        }
+        return output;
     }
 
     public Datapoint assignWeights(Datapoint datapoint) {
-        return null;
+        float sum = datapoint.distanceAP1 + datapoint.distanceAP2 + datapoint.distanceAP3 + datapoint.distanceAP4 + datapoint.distanceAP5;
+        datapoint.weightAP1 = datapoint.distanceAP1 / sum;
+        datapoint.weightAP2 = datapoint.distanceAP2 / sum;
+        datapoint.weightAP3 = datapoint.distanceAP3 / sum;
+        datapoint.weightAP4 = datapoint.distanceAP4 / sum;
+        datapoint.weightAP5 = datapoint.distanceAP5 / sum;
+        return datapoint;
     }
 }
---
+++ src/main/java/lsTriangulation.java
@@ -0,0 +1,12 @@
+/**
+ * Main class of the program. Handles the program flow.
+ * Created by carl on 16.08.16.
+ */
+public class lsTriangulation {
+
+    public static void main(String[] args) {
+        for (String s : args) {
+            System.out.println(s);
+        }
+    }
+}
